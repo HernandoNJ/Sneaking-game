611a73f089382c62efe9b6a27bb1847e38bef201
HernandoNJ <hernandonj.unity@gmail.com>
2022-07-13 17:57:38 -0500

    Making our Enemies Respond to the Player - finished

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -12,7 +12,6 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private float _threshold = 0.5f;
     [SerializeField] private float _waitTime = 2f;
 
-
     private Transform _currentPoint;
     private Vector3 _investigationPoint;
     private float _waitTimer;
@@ -28,49 +27,12 @@ public class EnemyController : MonoBehaviour
     private void Update()
     {
         if (_fov.visibleObjects.Count > 0)
-            InvestigatePoint(_fov.visibleObjects[0].position);
-
-        if (_state == EnemyState.Patrol)
-            UpdatePatrol();
-        else if (_state == EnemyState.Investigate)
-            UpdateInvestigate();
-    }
-
-    public void InvestigatePoint(Vector3 investigatePoint)
-    {
-        _state = EnemyState.Investigate;
-        _investigationPoint = investigatePoint;
-        _agent.SetDestination(_investigationPoint);
-    }
-
-    private void UpdateInvestigate()
-    {
-        if (Vector3.Distance(transform.position, _investigationPoint) < _threshold)
-        {
-            _waitTimer += Time.deltaTime;
-
-            if (_waitTimer > _waitTime) 
-                ReturnToPatrol();
-        }
-    }
-
-    private void ReturnToPatrol()
-    {
-        _state = EnemyState.Patrol;
-        _waitTimer = 0f;
-        _moving = false;
-    }
-
-    private void UpdatePatrol()
-    {
-        if (!_moving)
-        {
-            NextPatrolPoint();
-            _agent.SetDestination(_currentPoint.position);
-            _moving = true;
-        }
-
-        if (_moving && Vector3.Distance(transform.position, _currentPoint.position) < _threshold) { _moving = false; }
+        { InvestigatePoint(_fov.visibleObjects[0].position); }
+        
+        if (_state == EnemyState.Patrol) 
+        { UpdatePatrol(); }
+        else if (_state == EnemyState.Investigate) 
+        { UpdateInvestigate(); }
     }
 
     private void NextPatrolPoint()
@@ -91,5 +53,42 @@ public class EnemyController : MonoBehaviour
 
         if (_routeIndex == 0) _forwardsAlongPath = true;
         _currentPoint = _patrolRoute.route[_routeIndex];
+    }
+
+    public void InvestigatePoint(Vector3 investigatePoint)
+    {
+        _state = EnemyState.Investigate;
+        _investigationPoint = investigatePoint;
+        _agent.SetDestination(_investigationPoint);
+    }
+
+    private void UpdateInvestigate()
+    {
+        if (Vector3.Distance(transform.position, _investigationPoint) < _threshold)
+        {
+            _waitTimer += Time.deltaTime;
+            if (_waitTimer > _waitTime) 
+            { ReturnToPatrol(); }
+        }
+    }
+
+    private void ReturnToPatrol()
+    {
+        _state = EnemyState.Patrol;
+        _waitTimer = 0f;
+        _moving = false;
+    }
+
+    private void UpdatePatrol()
+    {
+        if (!_moving)
+        {
+            NextPatrolPoint();
+            _agent.SetDestination(_currentPoint.position);
+            _moving = true;
+        }
+
+        if (_moving && Vector3.Distance(transform.position, _currentPoint.position) < _threshold) 
+        { _moving = false; }
     }
 }
