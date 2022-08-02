ab2e10262f8ec079b9ab3792806235657a3c1a42
Hernando NJ <hernandonj.unity@gmail.com>
2022-08-01 22:46:36 -0500

    6.5. Adding our Wrist Radar finished

---
+++ Assets/Scripts/XR/MinimapRotator.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MinimapRotator : MonoBehaviour
+{
+    [SerializeField] private Transform rotationReference;
+    private Vector3 _initialRotation;
+
+    private void Start()
+    {
+        _initialRotation = transform.eulerAngles;
+    }
+
+    private void Update()
+    {
+        Vector3 newRot = new Vector3(0, 0, -rotationReference.eulerAngles.y) + _initialRotation;
+        transform.rotation = Quaternion.Euler(newRot);
+    }
+}
