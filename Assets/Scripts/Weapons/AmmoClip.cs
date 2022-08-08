2a1bdfcf939dea7ff5e03191be3c99d68e38bcce
Hernando NJ <hernandonj.unity@gmail.com>
2022-08-07 23:57:38 -0500

    7.1. Creating our Gun finished

---
+++ Assets/Scripts/Weapons/AmmoClip.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Weapons
+{
+public class AmmoClip : MonoBehaviour
+{
+    public GameObject bulletObject;
+    public float bulletSpeed = 20f;
+    public int amount = 3;
+}
+}
